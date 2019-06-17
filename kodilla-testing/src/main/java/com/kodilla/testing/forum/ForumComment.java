30b7970283e77dd9a6f60323947881e875bae280
szpila127 <inglot.seb@gmail.com>
2019-06-17 22:04:19 +0200

    6.4

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumComment.java
@@ -0,0 +1,45 @@
+package com.kodilla.testing.forum;
+
+import java.util.Objects;
+
+public class ForumComment {
+    ForumPost forumPost;
+    String commentBody;
+    String author;
+
+    public ForumComment(ForumPost forumPost, String commentBody, String author) {
+        this.forumPost = forumPost;
+        this.commentBody = commentBody;
+        this.author = author;
+    }
+
+    public ForumPost getForumPost() {
+        return forumPost;
+    }
+
+    public String getCommentBody() {
+        return commentBody;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ForumComment)) return false;
+        ForumComment that = (ForumComment) o;
+        return getForumPost().equals(that.getForumPost()) &&
+                getCommentBody().equals(that.getCommentBody()) &&
+                getAuthor().equals(that.getAuthor());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = forumPost.hashCode();
+        result = 31 * result + commentBody.hashCode();
+        result = 31 * result + author.hashCode();
+        return result;
+    }
+}
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumPost.java
@@ -0,0 +1,37 @@
+package com.kodilla.testing.forum;
+
+import java.util.Objects;
+
+public class ForumPost {
+    String postBody;
+    String author;
+
+    public ForumPost(String postBody, String author) {
+        this.postBody = postBody;
+        this.author = author;
+    }
+
+    public String getPostBody() {
+        return postBody;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ForumPost)) return false;
+        ForumPost forumPost = (ForumPost) o;
+        return getPostBody().equals(forumPost.getPostBody()) &&
+                getAuthor().equals(forumPost.getAuthor());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = postBody.hashCode();
+        result = 31 * result + author.hashCode();
+        return result;
+    }
+}
