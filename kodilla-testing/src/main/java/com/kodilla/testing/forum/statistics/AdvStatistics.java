9d07de374e0b463ce68c07f0af5c712d503f7958
szpila127 <inglot.seb@gmail.com>
2019-06-21 23:47:25 +0200

    6.5.Statistics

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/AdvStatistics.java
@@ -0,0 +1,71 @@
+package com.kodilla.testing.forum.statistics;
+
+public class AdvStatistics {
+    Statistics statistics;
+
+    int usersQuantity;
+    int postsQuantity;
+    int commentsQuantitny;
+    double avPostsPerUser;
+    double avCommentsPerUser;
+    double avCommentsPerPost;
+
+    public AdvStatistics(Statistics statistics) {
+        this.statistics = statistics;
+    }
+
+    public void calculateAdvStatistics(Statistics statistics) {
+
+        usersQuantity = statistics.usersNames().size();
+
+        postsQuantity = statistics.postsCount();
+
+        commentsQuantitny = statistics.commentsCount();
+
+        if (statistics.usersNames().size() == 0) {
+            avPostsPerUser = 0;
+        } else avPostsPerUser = (statistics.postsCount() / statistics.usersNames().size());
+
+        if (statistics.usersNames().size() == 0) {
+            avCommentsPerUser = 0;
+        } else avCommentsPerUser = (statistics.commentsCount() / statistics.usersNames().size());
+
+        if (statistics.postsCount() == 0) {
+            avCommentsPerPost = 0;
+        } else avCommentsPerPost = (statistics.commentsCount() / statistics.postsCount());
+    }
+
+    public String showStatistics() {
+
+        return "usersQuantity: " + usersQuantity +
+                "\n postsQuantity: " + postsQuantity +
+                "\n commentsQuantity: " + commentsQuantitny +
+                "\n avPostsPerUser: " + avPostsPerUser +
+                "\n avCommentsPerUser" + avCommentsPerUser +
+                "\n avCommentsPerPost" + avCommentsPerPost;
+    }
+
+    public int getUsersQuantity() {
+        return usersQuantity;
+    }
+
+    public int getPostsQuantity() {
+        return postsQuantity;
+    }
+
+    public int getCommentsQuantitny() {
+        return commentsQuantitny;
+    }
+
+    public double getAvPostsPerUser() {
+        return avPostsPerUser;
+    }
+
+    public double getAvCommentsPerUser() {
+        return avCommentsPerUser;
+    }
+
+    public double getAvCommentsPerPost() {
+        return avCommentsPerPost;
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/AdvStatisticsTestSuite.java
@@ -0,0 +1,56 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AdvStatisticsTestSuite {
+
+    private static int testCounter = 0;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @Test
+    public void calculateAdvStatisticsWithMock() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        List<String> staticticsList = new LinkedList<>();
+        staticticsList.add("Sebastian");
+        staticticsList.add("Michał");
+        int postsCount = 200;
+        int commentsCount = 1000;
+
+        when(statisticsMock.postsCount()).thenReturn(postsCount);
+        when(statisticsMock.commentsCount()).thenReturn(commentsCount);
+        when(statisticsMock.usersNames()).thenReturn(staticticsList);
+
+        AdvStatistics advStatistics = new AdvStatistics(statisticsMock);
+        //When
+        advStatistics.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(100, advStatistics.getAvPostsPerUser(), 0.01);
+//        Assert.assertEquals(50, advStatistics.getAvCommentsPerPost(), 0.01);
+//        Assert.assertEquals(500, advStatistics.getAvCommentsPerUser(), 0.01);
+//        Assert.assertEquals(500, advStatistics.getCommentsQuantitny());
+//        Assert.assertEquals(20, advStatistics.getPostsQuantity());
+//        Assert.assertEquals(2, advStatistics.getUsersQuantity());
+    }
+}
