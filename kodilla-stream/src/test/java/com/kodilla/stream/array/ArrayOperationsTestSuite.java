2d378b36a39453dbfde80bd47de47368d082249b
szpila127 <inglot.seb@gmail.com>
2019-06-26 21:03:47 +0200

    7.5.average

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -8,13 +8,15 @@ public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage() {
         //Given
-//        int[] numbers = {5, 8, 91, 73, 4, 4, 41, 93, 65, 39, 77, 39, 1, 8, 66, 88, 9, 45, 11, 1};
-        int[] numbers = new int[0];
+        int[] numbers = {5, 8, 91, 73, 4, 4, 41, 93, 65, 39, 77, 39, 1, 8, 66, 88, 9, 45, 11, 1};
+        int[] numbers2 = new int[0];
         //When
         double average = ArrayOperations.getAverage(numbers);
+        double average2 = ArrayOperations.getAverage(numbers2);
 
         //Then
-        Assert.assertEquals(0, average, 0.01);
+        Assert.assertEquals(38.4, average, 0.01);
+        Assert.assertEquals(0, average2, 0.01);
 
     }
 }
