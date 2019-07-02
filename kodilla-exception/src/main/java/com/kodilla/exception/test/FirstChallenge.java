3b459c3e312cda1a02e98a02ca1b8c8aa79de0a4
szpila127 <inglot.seb@gmail.com>
2019-07-02 22:34:40 +0200

    8.2.try.catch

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -3,21 +3,12 @@ package com.kodilla.exception.test;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-        try {
-
-            if (b == 0) {
-                throw new ArithmeticException();
-            }
-
-        } catch (ArithmeticException e) {
-
-            System.err.println("Nie możesz dzielić przez 0");
-
-        } finally {
-
-            return a / b;
+        if (b == 0) {
+            throw new ArithmeticException();
         }
+        return a / b;
     }
+
 
     /**
      * This main can throw an ArithmeticException!!!
@@ -26,11 +17,18 @@ public class FirstChallenge {
      */
     public static void main(String[] args) {
 
-        FirstChallenge firstChallenge = new FirstChallenge();
+        try {
 
-        double result = firstChallenge.divide(3, 0);
+            FirstChallenge firstChallenge = new FirstChallenge();
 
-        System.out.println(result);
+            double result = firstChallenge.divide(3, 0);
 
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Nie dzielimy przez 0");
+
+        }
     }
 }
