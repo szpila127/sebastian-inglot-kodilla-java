831ff83ba652d7bac0c75f5c9dc300386caad0ae
szpila127 <inglot.seb@gmail.com>
2019-08-25 14:29:47 +0200

    14.1 SpringIntro

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/pring/intro/Shape/Circle.java
@@ -0,0 +1,9 @@
+package com.kodilla.pring.intro.Shape;
+
+public class Circle extends Figure {
+
+    @Override
+    public void draw() {
+        System.out.println("This is a circle");
+    }
+}
--- kodilla-spring-intro/src/main/java/com/kodilla/pring/intro/Shape/Figure.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/pring/intro/Shape/Figure.java
@@ -1,4 +1,8 @@
 package com.kodilla.pring.intro.Shape;
 
 public class Figure {
+
+    public void draw() {
+        System.out.println("This is a geometric figure");
+    }
 }
