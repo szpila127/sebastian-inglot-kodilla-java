dfddfe57695b9ffa0ce0135557bb128e14d273fc
szpila127 <inglot.seb@gmail.com>
2019-08-27 19:31:08 +0200

    14.3 BeanConfiguration

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.library;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
