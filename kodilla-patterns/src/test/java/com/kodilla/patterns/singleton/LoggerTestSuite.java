439f1c7f7bf28178bc3cfa2af0c8fc54c98334fb
szpila127 <inglot.seb@gmail.com>
2019-08-30 20:04:33 +0200

    15.1 Singleton

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertSame;
+
+public class LoggerTestSuite {
+
+    @BeforeClass
+    public static void openLogger() {
+        Logger.getInstance();
+    }
+
+    @Test
+    public void testGetLastLog() {
+        //Given
+        //When
+        Logger.getInstance().log("test1");
+        String log = Logger.getInstance().getLastLog();
+        //Then
+        Assert.assertEquals("test1", log);
+    }
+}
