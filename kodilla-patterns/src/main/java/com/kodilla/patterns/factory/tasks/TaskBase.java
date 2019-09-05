0f6556114b47883ded8f91c970d32289f5a7e0d4
szpila127 <inglot.seb@gmail.com>
2019-09-05 20:04:55 +0200

    15.4 Factory

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/TaskBase.java
@@ -0,0 +1,7 @@
+package com.kodilla.patterns.factory.tasks;
+
+public enum TaskBase {
+    DRIVING_TASK,
+    PAINTING_TASK,
+    SHOPPING_TASK
+}
--- kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/TaskFactory.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/TaskFactory.java
@@ -1,12 +1,9 @@
 package com.kodilla.patterns.factory.tasks;
 
 public final class TaskFactory {
-    public static final String DRIVING_TASK = "DRIVING_TASK";
-    public static final String PAINTING_TASK = "PAINTING_TASK";
-    public static final String SHOPPING_TASK = "SHOPPING_TASK";
 
-    public final TaskInterface makeTask(final String taskClass) {
-        switch (taskClass) {
+    public final TaskInterface makeTask(TaskBase taskBase) {
+        switch (taskBase) {
             case DRIVING_TASK:
                 return new DrivingTask("Drive fast", "Krakow", "AUDI");
             case PAINTING_TASK:
