9420d7a43a7a79cf37e0daf64eac00b2a632f86b
szpila127 <inglot.seb@gmail.com>
2019-12-14 22:42:22 +0100

    1

--- src/main/java/com/kodilla/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KodillaCourseApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KodillaCourseApplication.class, args);
+    public static void main(String[] args) { SpringApplication.run(KodillaCourseApplication.class, args);
     }
-
 }
