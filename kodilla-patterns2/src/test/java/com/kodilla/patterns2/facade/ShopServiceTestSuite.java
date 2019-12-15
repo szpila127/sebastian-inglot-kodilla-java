500a0d10bbc611a47d43ca44bd1ea5a6d28b19cb
szpila127 <inglot.seb@gmail.com>
2019-12-15 11:42:15 +0100

    Patterns 2

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.patterns2.facade;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
